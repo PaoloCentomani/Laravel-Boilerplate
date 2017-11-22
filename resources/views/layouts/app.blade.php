56a16ebfde8bcd606f71304a864c4f8f9ba933e1
Paolo Centomani <paolo.centomani@gmail.com>
2017-11-22 19:36:11 +0100

    Configure Laravel Mix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -75,6 +75,8 @@
     </div>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ mix('js/manifest.js') }}"></script>
+    <script src="{{ mix('js/vendor.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,12 @@ let mix = require('laravel-mix');
  |
  */
 
+mix.autoload({
+    jquery: ['$', 'jQuery', 'window.jQuery']
+});
+
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .extract(['axios', 'bootstrap-sass', 'jquery', 'lodash', 'vue'])
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .sourceMaps()
+   .version();
