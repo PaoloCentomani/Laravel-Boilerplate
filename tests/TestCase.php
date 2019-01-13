36fcdffb9740c882f2aa631af28351ac517499fd
Paolo Centomani <paolo.centomani@gmail.com>
2019-01-13 13:41:47 +0100

    Testing helpers

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,21 @@
 
 namespace Tests;
 
+use App\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Sign in the user.
+     *
+     * @param  \App\User  $user
+     * @return $this
+     */
+    protected function signIn(User $user = null)
+    {
+        return $this->actingAs($user ?? factory(User::class)->create());
+    }
 }
