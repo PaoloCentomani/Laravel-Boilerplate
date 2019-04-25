1875545ed0aabd18f429db45a38a8d7df694a2e7
Paolo Centomani <paolo.centomani@gmail.com>
2019-04-25 15:34:33 +0200

    Configure Deployer

---
+++ deploy.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name.
+set('application', 'example.com');
+
+// Project repository.
+set('repository', 'git@github.com:PaoloCentomani/Laravel-Boilerplate.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys.
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server.
+add('writable_dirs', []);
+
+// Additional configuration.
+add('allow_anonymous_stats', false);
+set('http_group', 'www-data');
+set('writable_mode', 'chgrp');
+set('writable_use_sudo', true);
+
+// Hosts.
+
+host('example.com')
+    ->stage('production')
+    ->set('deploy_path', '/var/www/{{application}}');
+
+host('example.com')
+    ->stage('staging')
+    ->set('deploy_path', '/var/www/staging.{{application}}');
+
+// Tasks.
+
+// [Optional] If deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+before('deploy:symlink', 'artisan:migrate');
+
+// Deploy production assets.
+task('deploy:assets', function () {
+    runLocally('npm run prod');
+    upload('public/css/', '{{release_path}}/public/css/');
+    upload('public/img/', '{{release_path}}/public/img/');
+    upload('public/js/', '{{release_path}}/public/js/');
+    upload('public/mix-manifest.json', '{{release_path}}/public/');
+});
+after('deploy:vendors', 'deploy:assets');
