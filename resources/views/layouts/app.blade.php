0a7568ec42ab02615642aab29a51d6c3038dc615
Paolo Centomani <paolo.centomani@gmail.com>
2019-07-13 16:51:38 +0200

    PWA updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,10 +13,9 @@
 
     {{-- Web App --}}
     <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
     <meta name="apple-mobile-web-app-title" content="{{ config('app.name') }}">
-    <link rel="apple-touch-startup-image" href="{{ url('apple-touch-startup-image.png') }}">
-    <link rel="manifest" href="{{ url('site.webmanifest') }}">
+    <link rel="manifest" href="{{ url('manifest.json') }}">
     <link rel="mask-icon" href="{{ url('safari-pin-icon.svg') }}" color="#000">
 
     {{-- CSRF Token --}}
