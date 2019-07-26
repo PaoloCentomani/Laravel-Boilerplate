25af046e7eba294ab4cf424028cda350d541b2a6
Paolo Centomani <paolo.centomani@gmail.com>
2019-07-27 00:13:27 +0200

    Use Tailwind classes for focus box shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   theme: {
     container: {
-      padding: '1rem',
+      padding: '1rem'
     },
-    extend: {}
+    extend: {
+      boxShadow: {
+        glare: 'inset 0 1px 0 rgba(255, 255, 255, 0.15)',
+        outline: 'inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 0 0.2rem rgba(99, 179, 237, 0.25)'
+      }
+    }
   },
   variants: {},
   plugins: []
