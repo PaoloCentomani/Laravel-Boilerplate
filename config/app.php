e6f9869fe7fb1771e7ded667d4084db9bcb34448
Paolo Centomani <paolo.centomani@gmail.com>
2021-03-06 17:29:43 +0100

    Install Laravel Jetstream

--- config/app.php
+++ config/app.php
@@ -173,6 +173,8 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\FortifyServiceProvider::class,
+        App\Providers\JetstreamServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
     ],
